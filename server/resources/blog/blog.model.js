44daf012de5d8ad2d1320da5c4cfc1cb02e38b82
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-05-20 00:14:12 +0530

    Added fields

---
+++ server/resources/blog/blog.model.js
@@ -0,0 +1,8 @@
+/**
+ * Fields
+ * - Title
+ * - summary
+ * - body
+ * - featured
+ *
+ */
---
+++ server/resources/project/project.model.js
@@ -0,0 +1,12 @@
+/**
+ * Fields
+ * - title
+ * - catchLine ?
+ * - summary
+ * - link
+ * - techStack
+ * - tags []
+ * - githubLink
+ * - description: string
+ * - featured
+ */
