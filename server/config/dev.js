97d5c0f4b2163f14b6791d2e2d75cf1d08063627
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-05-20 00:17:21 +0530

    Updated Mongo URL

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,7 @@
 const config = {
   port: 8080,
   mongo: {
-    url: "mongodb://localhost:27017/portfolio",
+    url: "mongodb://localhost:27017/portfolioDev",
     options: { useNewUrlParser: true, useUnifiedTopology: true },
   },
 };
