330304042dd3bfd93af2c68bb8aa0a0501159903
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-05-19 23:40:31 +0530

    Added helmet

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const app = express();
 const { json, urlencoded } = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
 
 app.disable("x-powered-by");
+app.use(helmet());
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
