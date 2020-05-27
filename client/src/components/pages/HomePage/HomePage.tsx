15f705930158a41fb1c3571bd941d4f4674db5c6
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-05-27 20:26:03 +0530

    Added Helmet for SEO

--- client/src/components/pages/HomePage/HomePage.tsx
+++ client/src/components/pages/HomePage/HomePage.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function HomePage() {
-  console.log(1);
-  return <div>Home Page</div>;
+  return (
+    <>
+      <Helmet>
+        <title> Madhav Sharma :: Home Page</title>
+        <meta
+          name="description"
+          content="I’ve carved myself a little corner on the internet. I post interesting projects and articles here. Let me show you around."
+        />
+      </Helmet>
+      <div>Home Page</div>
+    </>
+  );
 }
 
 export default HomePage;
